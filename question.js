e1db9decd65908aa2cf9201299277dd9aab8112e
Mutasim fuad Rafi <165637733+mfbhai10@users.noreply.github.com>
2025-05-05 00:50:18 +0600

    Update question.js

--- question.js
+++ question.js
@@ -1,292 +1,50 @@
 let questions = [
-    {
-        numb: 1,
-        question: "শক্তির মূল উৎস কী?",
-        answer: "B. সূর্য",
-        options: [
-            "A. আলো",
-            "B. সূর্য",
-            "C. বিদ্যুৎ",
-            "D. তাপ"
-        ]
-    },
-    {
-        numb: 2,
-        question: "রিচার্জেবল ব্যাটারি বলা হয় কাকে?",
-        answer: "C. ATP",
-        options: [
-            "A. ADP",
-            "B. AMP",
-            "C. ATP",
-            "D. NADPH2"
-        ]
-    },
-    {
-        numb: 3,
-        question: "Biological coin এর বাংলা কী?",
-        answer: "B. জৈব মুদ্রা",
-        options: [
-            "A. জীব মুদ্রা",
-            "B. জৈব মুদ্রা",
-            "C. অজৈব মুদ্রা",
-            "D. শক্তি মুদ্রা"
-        ]
-    },
-    {
-        numb: 4,
-        question: "ATP কে ভেঙে কোনটি তৈরি করে?",
-        answer: "A. ADP",
-        options: [
-            "A. ADP",
-            "B. NAD",
-            "C. NADH2",
-            "D. GTP"
-        ]
-    },
-    {
-        numb: 5,
-        question: "পত্ররন্ধ্রের মাধ্যমে সবুজ উদ্ভিদ বায়ু থেকে কী গ্রহণ করে?",
-        answer: "B. CO2",
-        options: [
-            "A. O2",
-            "B. CO2",
-            "C. H2",
-            "D. কোনটিই নয়"
-        ]
-    },
-    {
-        numb: 6,
-        question: "সালোকসংশ্লেষণ প্রক্রিয়ায় উপজাত হিসেবে নির্গত হয় কোনটি?",
-        answer: "C. অক্সিজেন",
-        options: [
-            "A. পানি",
-            "B. শর্করা",
-            "C. অক্সিজেন",
-            "D. কার্বন ডাইঅক্সাইড"
-        ]
-    },
-    {
-        numb: 7,
-        question: "ফটোফসফোরাইলেশন প্রক্রিয়ায় কী তৈরি হয়?",
-        answer: "A. ATP",
-        options: [
-            "A. ATP",
-            "B. ADP",
-            "C. GTP",
-            "D. NAD"
-        ]
-    },
-    {
-        numb: 8,
-        question: "বায়ুমমণ্ডলের CO2 কিসের মধ্যে দিয়ে কোষে প্রবেশ করে?",
-        answer: "A. পাতা",
-        options: [
-            "A. পাতা",
-            "B. কাণ্ড",
-            "C. মূল",
-            "D. শাখা-প্রশাখা"
-        ]
-    },
-    {
-        numb: 9,
-        question: "ক্যালভিন চক্রে অস্থায়ী কী তৈরি হয়?",
-        answer: "B. কিটোএসিড",
-        options: [
-            "A. অ্যামাইনো এসিড",
-            "B. কিটোএসিড",
-            "C. এসিটিক এসিড",
-            "D. ফসফোরিক এসিড"
-        ]
-    },
-    {
-        numb: 10,
-        question: "C3 গতিপথ আবিষ্কারকের নাম কী?",
-        answer: "A. ক্যালভিন বেনসন",
-        options: [
-            "A. ক্যালভিন বেনসন",
-            "B. ক্যালভিন হ্যাব্স",
-            "C. রবার্ট বেনসন",
-            "D. ক্যালভিন মুলার"
-        ]
-    },
-    {
-        numb: 11,
-        question: "C4 চক্রপরিচালিত হয় কোন উদ্ভিদে?",
-        answer: "A. মুথা ঘাস",
-        options: [
-            "A. মুথা ঘাস",
-            "B. আমগাছ",
-            "C. জামগাছ",
-            "D. সাইকাস"
-        ]
-    },
-    {
-        numb: 12,
-        question: "বাতাসে কোন গ্যাসটির উপস্থিতি সালোকসংশ্লেষণে ব্যাঘাত ঘটায়?",
-        answer: "B. CH4",
-        options: [
-            "A. CO2",
-            "B. CH4",
-            "C. N2",
-            "D. H2"
-        ]
-    },
-    {
-        numb: 13,
-        question: "উদ্ভিদের বাহ্যিক প্রভাবক কোনটি?",
-        answer: "C. খনিজ পদার্থ",
-        options: [
-            "A. ক্লোরোফিল",
-            "B. এনজাইম",
-            "C. খনিজ পদার্থ",
-            "D. হাইড্রোজেন"
-        ]
-    },
-    {
-        numb: 14,
-        question: "কোন তাপমাত্রায় সালোকসংশ্লেষণ বন্ধ হয়ে যায়?",
-        answer: "D. ৫০° C",
-        options: [
-            "A. ৩০°",
-            "B. ২৫° C",
-            "C. ১৭°",
-            "D. ৫০° C"
-        ]
-    },
-    {
-        numb: 15,
-        question: "পাতার কোন অংশে ক্লোরোপ্লাস্ট সবচেয়ে বেশি থাকে?",
-        answer: "B. প্যালিসেড প্যারানকাইমা",
-        options: [
-            "A. স্পঞ্জি প্যারানকাইমা",
-            "B. প্যালিসেড প্যারানকাইমা",
-            "C. জাইলেম টিস্যু",
-            "D. নিম্নত্বক"
-        ]
-    },
-    {
-        numb: 16,
-        question: `সালোকসংশ্লেষণ প্রক্রিয়ায় উৎপন্ন হয়-
-i. H2O
-ii. CO2
-iii. O2`,
-        answer: "B. i ও iii",
-        options: [
-            "A. i ও ii",
-            "B. i ও iii",
-            "C. ii ও iii ",
-            "D. i, ii ও iii"
-        ]
-    },
-    {
-        numb: 17,
-        question: "শ্বসনিক বস্তু নয় কোনটি?",
-        answer: "D. খনিজ লবণ",
-        options: [
-            "A. জৈব এসিড",
-            "B. শর্করা",
-            "C. লিপিড",
-            "D. খনিজ লবণ"
-        ]
-    },
-    {
-        numb: 18,
-        question: "শ্বসনের গ্লাইকোলাইসিস প্রক্রিয়ায় কত অণু ATP তৈরি হয়?",
-        answer: "A. ৪",
-        options: [
-            "A. ৪",
-            "B. ৬",
-            "C. ৮",
-            "D. ১৮"
-        ]
-    },
-    {
-        numb: 19,
-        question: "শ্বসন প্রক্রিয়ায় কয় অণু পানি উৎপন্ন হয়?",
-        answer: "D. ৬ অণু",
-        options: [
-            "A. ২ অণু",
-            "B. ৪ অণু",
-            "C. ৫ অণু",
-            "D. ৬ অণু"
-        ]
-    },
-    {
-        numb: 20,
-        question: "সবাত শ্বসন প্রক্রিয়ায় সর্বমোট কত অণু ATP উৎপন্ন করে?",
-        answer: "B. ৩৮",
-        options: [
-            "A. ৩০",
-            "B. ৩৮",
-            "C. ৩২",
-            "D. ৩৩"
-        ]
-    },
-    {
-        numb: 21,
-        question: "কোন বিভাজনের প্রয়োজনীয় শক্তি আসে কোন প্রক্রিয়া হতে?",
-        answer: "D. শ্বসন",
-        options: [
-            "A. প্রস্বেদন",
-            "B. অভিস্রবণ",
-            "C. ব্যাপন",
-            "D. শ্বসন"
-        ]
-    },
-    {
-        numb: 22,
-        question: `আত্মীকরণ শক্তি বলা হয়- নিচের কোনটি সঠিক?
-i. ATP
-ii. FADH2
-iii. NADPH2`,
-        answer: "D. i, ii ও iii",
-        options: [
-            "A. i ও ii",
-            "B. i ও iii",
-            "C. ii ও iii",
-            "D. i, ii ও iii"
-        ]
-    },
-    {
-        numb: 23,
-        question: `ক্রেবস চক্রে উৎপন্ন হয়- নিচের কোনটি সঠিক?
-i. তিন অণু NADH2
-ii. এক অণু GTP
-iii. এক অণু FADH2`,
-        answer: "D. i, ii ও iii",
-        options: [
-            "A. i ও ii",
-            "B. i ও iii",
-            "C. ii ও iii",
-            "D. i, ii ও iii"
-        ]
-    },
-    {
-        numb: 24,
-        question: `পানির ফটোলাইসিস প্রক্রিয়ায় উৎপন্ন হয়- নিচের কোনটি সঠিক?
-i. অক্সিজেন
-ii. কার্বন ডাইঅক্সাইড
-iii. ইলেকট্রন`,
-        answer: "B. i ও iii",
-        options: [
-            "A. i ও ii",
-            "B. i ও iii",
-            "C. ii ও iii",
-            "D. i, ii ও iii"
-        ]
-    },
-    {
-        numb: 25,
-        question: `সবুজ উদ্ভিদের সালোকসংশ্লেষণিক অঙ্গ- নিচের কোনটি সঠিক?
-i. সবুজ পাতা
-ii. সবুজ কাণ্ড
-iii. সবুজ মূল`,
-        answer: "A. i ও ii",
-        options: [
-            "A. i ও ii",
-            "B. i ও iii",
-            "C. ii ও iii",
-            "D. i, ii ও iii"
-        ]
-    }
+  {
+    numb: 1,
+    question: "Which of the following is true for variable names in Python?",
+    answer: "unlimited length",
+    options: [
+      "underscore and ampersand are the only two special characters allowed",
+      "unlimited length",
+      "all private members must have leading and trailing underscores",
+      "none of the mentioned",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a type of database?",
+    answer: "Decentralized",
+    options: ["Hierarchical", "Network", "Distributed", "Decentralized"],
+  },
+  {
+    numb: 3,
+    question:
+      "Suppose you knows Selection sort, Insertion sort & Quick sort which one he wil used for best time complexity ?",
+    answer: "Quick sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Which of the following is known as a set of entities of the same type that share same properties, or attributes?",
+    answer: "Entity set",
+    options: ["Relation set", "Tuples", "Entity set", "Entity Relation model"],
+  },
+  {
+    numb: 5,
+    question: "Which command is used to remove a relation from an SQL ?",
+    answer: "Drop table",
+    options: ["Drop table", "Delete", "Purge", "Remove"],
+  },
+  {
+    numb: 6,
+    question: "Please, Rate this Portal!",
+    answer: "None",
+    options: ["Good", "Need some improvement", "Not good", "None"],
+  },
 ];
